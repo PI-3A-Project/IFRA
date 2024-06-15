5ffefcf5901bf65fbb55be9ca826c6a997b8e069
Djneto <dijalma.bneto@gmail.com>
2024-06-15 19:40:36 -0300

    Bottom Navigation.

--- app/src/main/java/br/com/ifra/MainActivity.java
+++ app/src/main/java/br/com/ifra/MainActivity.java
@@ -2,18 +2,17 @@ package br.com.ifra;
 
 import android.os.Bundle;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import br.com.ifra.R;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.color.DynamicColors;
-import com.google.android.material.progressindicator.CircularProgressIndicator;
-import com.google.android.material.search.SearchBar;
-import com.google.android.material.search.SearchView;
-
-import br.com.ifra.search.ProgressoBusca;
-import br.com.ifra.search.SearchUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,24 +21,32 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.cat_search_fragment);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        frameLayout = findViewById(R.id.main_frame);
 
-        CircularProgressIndicator progressIndicator = findViewById(R.id.circular_indicator);
-        ProgressoBusca.setProgressIndicator(progressIndicator);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.search){
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new SearchFragment()).commit();
+                //Toast.makeText(MainActivity.this, "Pesquisar clicado", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            else if (itemId == R.id.books){
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new BooksFragment()).commit();
+                //Toast.makeText(MainActivity.this, "Livros clicado", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return false;
+        });
 
-        SearchBar searchBar = findViewById(R.id.cat_search_bar);
-        SearchView searchView = findViewById(R.id.cat_search_view);
-        LinearLayout suggestionContainer = findViewById(R.id.cat_search_view_suggestion_container);
-
-        SearchUtils.setUpSearchBar(this, searchBar);
-        SearchUtils.setUpSearchView(suggestionContainer, this, searchBar, searchView);
-        SearchUtils.setUpSuggestions(suggestionContainer, searchBar, searchView);
-        SearchUtils.startOnLoadAnimation(searchBar, savedInstanceState);
+        // Set the default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new SearchFragment()).commit();
+        }
 
         DynamicColors.applyToActivitiesIfAvailable(this.getApplication(), R.style.AppTheme);
     }
 }
-
--- app/src/main/java/br/com/ifra/SearchFragment.java
+++ app/src/main/java/br/com/ifra/SearchFragment.java
@@ -1,76 +1,45 @@
 package br.com.ifra;
 
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.android.material.search.SearchBar;
 import com.google.android.material.search.SearchView;
 
+import br.com.ifra.search.ProgressoBusca;
 import br.com.ifra.search.SearchUtils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SearchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SearchFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.cat_search_fragment, container, false);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.cat_search_fragment, container, false);
+        CircularProgressIndicator progressIndicator = view.findViewById(R.id.circular_indicator);
+        ProgressoBusca.setProgressIndicator(progressIndicator);
 
         SearchBar searchBar = view.findViewById(R.id.cat_search_bar);
         SearchView searchView = view.findViewById(R.id.cat_search_view);
         LinearLayout suggestionContainer = view.findViewById(R.id.cat_search_view_suggestion_container);
 
-        SearchUtils.setUpSearchBar(requireActivity(), searchBar);
+        SearchUtils.setUpSearchBar(getActivity(), searchBar);
+        SearchUtils.setUpSearchView(suggestionContainer, (AppCompatActivity) getActivity(), searchBar, searchView);
+        SearchUtils.setUpSuggestions(suggestionContainer, searchBar, searchView);
         SearchUtils.startOnLoadAnimation(searchBar, savedInstanceState);
-
-        return view;
     }
 }
